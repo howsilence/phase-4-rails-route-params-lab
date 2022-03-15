cda1bfbe3ef137342f986fe9f4c2d8719bff2d44
howsilence <statton0104@gmail.com>
2022-03-15 14:35:28 -0400

    commit tree 07ff52cec38a1f27712f12d655f39da191bdac11

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+      Student.by_name(params[:name])
+    else 
+      Student.all
+    end
+    render json: students
+  end
+
+  def findbyid
+    students = Student.find_by(id: params[:id])
     render json: students
   end
 
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ApplicationRecord
+  #this uses scope, the lambda starts the code, this reads:
+  #call scope, name this by_name, ->(name) look at the name attribute {where("the first_name attribute is present,  OR the last_name attribute is present", **? acts as a placeholder** then i THINK the {name}%, is saying if there is any leftovers from the name its not it )}
+  scope :by_name, ->(name) { where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%") }
+
 
   def to_s
     "#{first_name} #{last_name}"
